ab4680a8f2fc2e2cac9b86da45d8f9499b0c63e3
Max <bludux595@gmail.com>
2020-10-03 19:16:54 -0400

    Model changes

---
+++ src/main/java/blueduck/mysticalpumpkins/block/MysticalPumpkinItem.java
@@ -0,0 +1,10 @@
+package blueduck.mysticalpumpkins.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+
+public class MysticalPumpkinItem extends BlockItem {
+    public MysticalPumpkinItem(Block blockIn, Properties builder) {
+        super(blockIn, builder);
+    }
+}
