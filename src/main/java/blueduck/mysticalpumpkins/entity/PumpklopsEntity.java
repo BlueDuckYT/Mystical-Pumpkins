955697b38c24d58540c786a551de80ccf9952e72
AzureZhen <rcraft@cyberconnects2.com>
2020-11-29 20:03:29 -0500

    Pumpklops updated

--- src/main/java/blueduck/mysticalpumpkins/entity/PumpklopsEntity.java
+++ src/main/java/blueduck/mysticalpumpkins/entity/PumpklopsEntity.java
@@ -1,9 +1,21 @@
 package blueduck.mysticalpumpkins.entity;
 
-import net.minecraft.entity.*;
+import javax.annotation.Nullable;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.IRangedAttackMob;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.ai.attributes.AttributeModifierMap;
 import net.minecraft.entity.ai.attributes.Attributes;
-import net.minecraft.entity.ai.goal.*;
+import net.minecraft.entity.ai.goal.HurtByTargetGoal;
+import net.minecraft.entity.ai.goal.LookAtGoal;
+import net.minecraft.entity.ai.goal.LookRandomlyGoal;
+import net.minecraft.entity.ai.goal.MeleeAttackGoal;
+import net.minecraft.entity.ai.goal.NearestAttackableTargetGoal;
+import net.minecraft.entity.ai.goal.RangedAttackGoal;
+import net.minecraft.entity.ai.goal.WaterAvoidingRandomWalkingGoal;
 import net.minecraft.entity.monster.MonsterEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
@@ -15,155 +27,146 @@ import net.minecraft.util.text.ITextComponent;
 import net.minecraft.world.BossInfo;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerBossInfo;
-import software.bernie.geckolib.animation.builder.AnimationBuilder;
-import software.bernie.geckolib.animation.controller.AnimationController;
-import software.bernie.geckolib.animation.controller.EntityAnimationController;
-import software.bernie.geckolib.entity.IAnimatedEntity;
-import software.bernie.geckolib.event.AnimationTestEvent;
-import software.bernie.geckolib.manager.EntityAnimationManager;
+import software.bernie.geckolib3.core.IAnimatable;
+import software.bernie.geckolib3.core.PlayState;
+import software.bernie.geckolib3.core.builder.AnimationBuilder;
+import software.bernie.geckolib3.core.controller.AnimationController;
+import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
+import software.bernie.geckolib3.core.manager.AnimationData;
+import software.bernie.geckolib3.core.manager.AnimationFactory;
 
-import javax.annotation.Nullable;
+public class PumpklopsEntity extends MonsterEntity implements IAnimatable, IRangedAttackMob {
 
-public class PumpklopsEntity extends MonsterEntity implements IAnimatedEntity, IRangedAttackMob {
+	private final ServerBossInfo bossInfo = (ServerBossInfo) (new ServerBossInfo(this.getDisplayName(),
+			BossInfo.Color.YELLOW, BossInfo.Overlay.PROGRESS)).setDarkenSky(true);
 
-    private final ServerBossInfo bossInfo = (ServerBossInfo)(new ServerBossInfo(this.getDisplayName(), BossInfo.Color.YELLOW, BossInfo.Overlay.PROGRESS)).setDarkenSky(true);
+	public int attackTimer = 0;
 
-    public EntityAnimationManager animationManager = new EntityAnimationManager();
+	public PumpklopsEntity(EntityType<? extends MonsterEntity> type, World worldIn) {
+		super(type, worldIn);
+		this.setHealth(this.getMaxHealth());
+	}
 
-    private final AnimationController<PumpklopsEntity> moveController = new EntityAnimationController<>(this, "moveController", 10F, this::moveController);
+	private AnimationFactory factory = new AnimationFactory(this);
 
-    public int attackTimer = 0;
+	private <E extends IAnimatable> PlayState predicate(AnimationEvent<E> event) {
+		boolean b = false;
+		if (attackTimer > 0) {
+			//event.getController().setAnimation(new AnimationBuilder().addAnimation("attack", true));
+			attackTimer--;
+			b = true;
+			return PlayState.CONTINUE;
+		} else if (event.isMoving()) {
+			event.getController().setAnimation(new AnimationBuilder().addAnimation("move", true));
+			return PlayState.CONTINUE;
+		}
+		return PlayState.STOP;
+	}
 
-    public PumpklopsEntity(EntityType<? extends MonsterEntity> type, World worldIn) {
-        super(type, worldIn);
-        registerAnimationControllers();
-        this.setHealth(this.getMaxHealth());
-    }
+	@Override
+	public void registerControllers(AnimationData data) {
+		data.addAnimationController(
+				new AnimationController<PumpklopsEntity>(this, "controller", 0.1F, this::predicate));
+	}
 
-    public void registerAnimationControllers() {
-        if(world.isRemote) {
-            this.animationManager.addAnimationController(moveController);
-        }
-    }
+	@Override
+	public AnimationFactory getFactory() {
+		return this.factory;
+	}
 
-    @Override
-    public EntityAnimationManager getAnimationManager() {
-        return animationManager;
-    }
+	@Override
+	public void readAdditional(CompoundNBT compound) {
+		super.readAdditional(compound);
+		if (this.hasCustomName()) {
+			this.bossInfo.setName(this.getDisplayName());
+		}
 
-    private <T extends Entity> boolean moveController(AnimationTestEvent<T> event) {
-        boolean b = false;
-        if (attackTimer > 0) {
-            attackTimer--;
-            //moveController.setAnimation(new AnimationBuilder().addAnimation("attack", true));
-            b = true;
-            return true;
-        } else if (event.isWalking()) {
-            moveController.setAnimation(new AnimationBuilder().addAnimation("move", true));
-//            if (!b) {
-//
-//            }
-            return true;
-        }
-        return false;
-    }
+	}
 
-    @Override
-    public void readAdditional(CompoundNBT compound) {
-        super.readAdditional(compound);
-        if (this.hasCustomName()) {
-            this.bossInfo.setName(this.getDisplayName());
-        }
+	@Override
+	protected void updateAITasks() {
+		super.updateAITasks();
+		this.bossInfo.setPercent(this.getHealth() / this.getMaxHealth());
+	}
 
-    }
+	@Override
+	public void setCustomName(@Nullable ITextComponent name) {
+		super.setCustomName(name);
+		this.bossInfo.setName(this.getDisplayName());
+	}
 
-    @Override
-    protected void updateAITasks() {
-        super.updateAITasks();
-        this.bossInfo.setPercent(this.getHealth() / this.getMaxHealth());
-    }
+	@Override
+	public boolean attackEntityAsMob(Entity entityIn) {
+		attackTimer = 60;
+		return super.attackEntityAsMob(entityIn);
+	}
 
-    @Override
-    public void setCustomName(@Nullable ITextComponent name) {
-        super.setCustomName(name);
-        this.bossInfo.setName(this.getDisplayName());
-    }
+	@Override
+	public void addTrackingPlayer(ServerPlayerEntity player) {
+		super.addTrackingPlayer(player);
+		this.bossInfo.addPlayer(player);
+	}
 
-    @Override
-    public boolean attackEntityAsMob(Entity entityIn) {
-        attackTimer = 60;
-       // moveController.setAnimation(new AnimationBuilder().addAnimation("attack", true));
+	@Override
+	public void removeTrackingPlayer(ServerPlayerEntity player) {
+		super.removeTrackingPlayer(player);
+		this.bossInfo.removePlayer(player);
+	}
 
-        return super.attackEntityAsMob(entityIn);
-    }
+	@Override
+	protected void registerGoals() {
+		this.goalSelector.addGoal(1, new RangedAttackGoal(this, 1.0, 40, 8.0F));
+		this.goalSelector.addGoal(2, new MeleeAttackGoal(this, 1.0D, false));
+		this.goalSelector.addGoal(3, new WaterAvoidingRandomWalkingGoal(this, 1.0D));
+		this.goalSelector.addGoal(7, new LookAtGoal(this, PlayerEntity.class, 8.0F));
+		this.goalSelector.addGoal(8, new LookRandomlyGoal(this));
+		this.targetSelector.addGoal(1, (new HurtByTargetGoal(this)).setCallsForHelp());
+		this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, PlayerEntity.class, true));
+	}
 
-    @Override
-    public void addTrackingPlayer(ServerPlayerEntity player) {
-        super.addTrackingPlayer(player);
-        this.bossInfo.addPlayer(player);
-    }
+	public static AttributeModifierMap.MutableAttribute setCustomAttributes() {
+		return MobEntity.func_233666_p_().createMutableAttribute(Attributes.MAX_HEALTH, 500.0D) // health
+				.createMutableAttribute(Attributes.MOVEMENT_SPEED, 0.3D) // movement speed
+				.createMutableAttribute(Attributes.ATTACK_DAMAGE, 8.0D) // attack damage
+				.createMutableAttribute(Attributes.ATTACK_KNOCKBACK, 0.2D); // attack knockback
+	}
 
-    @Override
-    public void removeTrackingPlayer(ServerPlayerEntity player) {
-        super.removeTrackingPlayer(player);
-        this.bossInfo.removePlayer(player);
-    }
+	@Override
+	public SoundEvent getHurtSound(DamageSource damageSourceIn) {
+		return SoundEvents.ENTITY_ZOMBIE_HURT;
+	}
 
-    @Override
-    protected void registerGoals() {
-        this.goalSelector.addGoal(1, new RangedAttackGoal(this, 1.0, 40, 8.0F));
-        this.goalSelector.addGoal(2, new MeleeAttackGoal(this, 1.0D, false));
-        this.goalSelector.addGoal(3, new WaterAvoidingRandomWalkingGoal(this, 1.0D));
-        this.goalSelector.addGoal(7, new LookAtGoal(this, PlayerEntity.class, 8.0F));
-        this.goalSelector.addGoal(8, new LookRandomlyGoal(this));
-        this.targetSelector.addGoal(1, (new HurtByTargetGoal(this)).setCallsForHelp());
-        this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, PlayerEntity.class, true));
-    }
+	@Override
+	public SoundEvent getDeathSound() {
+		return SoundEvents.ENTITY_ZOMBIE_DEATH;
+	}
 
-    public static AttributeModifierMap.MutableAttribute setCustomAttributes() {
-        return MobEntity.func_233666_p_()
-                .createMutableAttribute(Attributes.MAX_HEALTH, 500.0D) //health
-                .createMutableAttribute(Attributes.MOVEMENT_SPEED, 0.3D) //movement speed
-                .createMutableAttribute(Attributes.ATTACK_DAMAGE, 8.0D) //attack damage
-                .createMutableAttribute(Attributes.ATTACK_KNOCKBACK, 0.2D); //attack knockback
-    }
+	private void launchMagicBallToEntity(LivingEntity entity) {
+		if (entity != null)
+			launchMagicBallToCoords(entity.getPosX(), entity.getPosY(), entity.getPosZ());
+	}
 
-    @Override
-    public SoundEvent getHurtSound(DamageSource damageSourceIn) {
-        return SoundEvents.ENTITY_ZOMBIE_HURT;
-    }
+	private void launchMagicBallToCoords(double x, double y, double z) {
+		if (!this.isSilent()) {
+			this.world.playEvent(null, 1024, this.getPosition(), 0);
+		}
 
-    @Override
-    public SoundEvent getDeathSound() {
-        return SoundEvents.ENTITY_ZOMBIE_DEATH;
-    }
+		// TODO getScepter coords
+		double d0 = this.getPosX();
+		double d1 = this.getPosYEye();
+		double d2 = this.getPosZ();
+		double d3 = x - d0;
+		double d4 = y - d1;
+		double d5 = z - d2;
+		GreenMagicBallEntity magic = new GreenMagicBallEntity(this, d3, d4, d5, this.world);
+		magic.setShooter(this);
+		magic.setRawPosition(d0, d1, d2);
+		this.world.addEntity(magic);
+	}
 
-    private void launchMagicBallToEntity(LivingEntity entity) {
-        if (entity != null)
-            launchMagicBallToCoords(entity.getPosX(), entity.getPosY(), entity.getPosZ());
-    }
-
-    private void launchMagicBallToCoords(double x, double y, double z) {
-        if (!this.isSilent()) {
-            this.world.playEvent(null, 1024, this.getPosition(), 0);
-        }
-
-        //TODO getScepter coords
-        double d0 = this.getPosX();
-        double d1 = this.getPosYEye();
-        double d2 = this.getPosZ();
-        double d3 = x - d0;
-        double d4 = y - d1;
-        double d5 = z - d2;
-        GreenMagicBallEntity magic = new GreenMagicBallEntity(this, d3, d4, d5, this.world);
-        magic.setShooter(this);
-        magic.setRawPosition(d0, d1, d2);
-        this.world.addEntity(magic);
-    }
-
-    @Override
-    public void attackEntityWithRangedAttack(LivingEntity target, float distanceFactor) {
-        attackTimer = 60;
-        launchMagicBallToEntity(target);
-    }
+	@Override
+	public void attackEntityWithRangedAttack(LivingEntity target, float distanceFactor) {
+		attackTimer = 60;
+		launchMagicBallToEntity(target);
+	}
 }
